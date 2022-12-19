c9b77ca1131f4591008990d59e7d2124c5e6cd01
the-cube0001 <120981804+the-cube0001@users.noreply.github.com>
2022-12-20 03:48:19 +1300

    Add files via upload

---
+++ install_dependencies.bat
@@ -0,0 +1,12 @@
+@echo off
+
+echo Installing required dependencies...
+
+pip install random
+pip install string
+pip install keyword
+pip install base64
+
+echo Dependencies installed successfully.
+
+pause
